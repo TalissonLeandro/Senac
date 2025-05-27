bc4ab029bc99c47090b95904b31aa7b7a88a293f
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-05-27 08:33:23 -0300

    Add files via upload

--- php/ex10.php
+++ php/ex10.php
@@ -4,7 +4,10 @@ $instrutor = "Rafael";
 $num1 = 5;
 $num2 = 12;
 $num3 = 11;
-$mult = $num1 * $num2 * $num3;
+$num4 = 13;
+$num5 = 1;
+
+$mult = $num1 * $num2 * $num3 * $num4 * $num5;
 
 echo "O nome do instrutor é: ".$instrutor;
 echo "O valor da multiplicação dos números é: ".$mult;
