b29388f9ac269dab873d36eaf73bfd44b9d0a248
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-05-27 09:44:46 -0300

    Add files via upload

---
+++ php/ex11.php
@@ -0,0 +1,11 @@
+<?php
+
+// Faça um código PHP que recebe o salário informado pelo usuário e calcula o salário aumentado em 30%
+
+$salario = 2000;
+$porcentagem = ((30*$salario)/100);
+$salario_novo = $salario + $porcentagem;
+
+echo "Após o ajuste, o salário ficará em: ".$salario_novo;
+
+?>
---
+++ php/ex12.php
@@ -0,0 +1,13 @@
+<?php
+
+// Faça um código PHP que permite ao usuário digitar um número e calcula o dobro e o triplo desse número.
+
+$numero = 5;
+$dobro = $numero + $numero;
+$triplo = $numero + $numero + $numero;
+
+echo "Numero informado: ".$numero;
+echo " O dobro do número é: ".$dobro;
+echo " O triplo do número é: ".$triplo;
+
+?>
