8c4402064b61f018ebc90ec9f3473ce5ad570639
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-05-29 09:13:57 -0300

    Add files via upload

---
+++ php/ex13.php
@@ -0,0 +1,12 @@
+<?php
+
+// Faça um código PHP que utiliza if e else para verificar se uma pessoa pode votar (idade >= 16)
+$idade = 16;
+
+if($idade >= 16){
+    echo "Você pode votar!";
+}else {
+    echo "Você não pode votar :<";
+}
+
+?>
