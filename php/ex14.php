792551e0f45b51d8ec6a703efe3c1eb4746b06fd
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-05-29 09:37:52 -0300

    Add files via upload

---
+++ php/ex14.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$num = 1;
+
+if($num < 0){
+    echo "O número é negativo!";
+}else if($num == 0){
+    echo "O número é igual a 0.";
+}else if($num > 0){
+    echo "O número é positivo!";
+}else{
+    "Digite um número válido";
+}
+
+?>
