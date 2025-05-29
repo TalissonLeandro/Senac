b1cb08b7959294401c2c8a174a61c6446b61df26
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-05-29 11:04:39 -0300

    Add files via upload

--- php/form/script.php
+++ php/form/script.php
@@ -2,18 +2,14 @@
 
 $nome = $_POST['nome'];
 $salario = (int) $_POST['salario'];
-$salario_aumento = (((30*$salario)/100) + $salario);
-$salario_com_vt = (6*($salario_aumento)/100);
 $empresa = $_POST['empresa'];
+$salario_aumento = (((30*$salario)/100) + $salario);
 
-if($salario_aumento > 1518){
-    echo "Olá ".$nome." salário informado: ".$salario;
-    echo "<br>"." Após o ajuste de 30%, o salário ficará em: ".$salario_aumento.", atualmente trabalha na empresa: ".$empresa;
-    echo "<br>"." com o desconto de VT, ficará em: ".$salario_com_vt;
-}
 
-//echo "Olá, ".$nome." salário informado: ".$salario;
-//echo "<br>"."Após o ajuste de 30%, o salário ficará em: ".$salario_aumento. " e atualmente trabalha na empresa ".$empresa;
+echo "Olá ".$nome." salário informado: ".$salario;
+echo "<br>"." Após o ajuste de 30%, o salário ficará em: ".$salario_aumento.", atualmente trabalha na empresa: ".$empresa;
+
+
 
 
 ?>
