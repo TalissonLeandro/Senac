642fc1d08342af6f1b003d97d3731997546ad121
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-06-03 08:55:13 -0300

    Add files via upload

--- php/ex17.php
+++ php/ex17.php
@@ -1,26 +1,26 @@
 <?php
 
-$nota1 = 5;
-$nota2 = 8;
-$nota3 = 4;
-$soma = $nota1 + $nota2 + $nota3;
-$nome = 'João';
-$idade = 15;
+$nome = ['nome'];
+$pressao = ['pressao'];
 
-$media = $soma / 3;
 
-if ($media < 4) {
-    echo $nome . " você tem " . $idade . " anos. Situação: Reprovado por desempenho insuficiente. A soma de suas notas é: " . $soma;
-} else if ($media >= 4 && $media < 6) {
-    echo $nome . " você tem " . $idade . " anos. Situação: Recuperação. A soma de suas notas é: " . $soma;
-} else if ($media >= 6 && $media < 8) {
-    echo $nome . " você tem " . $idade . " anos. Situação: Aprovado com desempenho regular. A soma de suas notas é: " . $soma;
-} else if ($media >= 8 && $media < 9.5) {
-    echo $nome . " você tem " . $idade . " anos. Situação: Aprovado com bom desempenho! A soma de suas notas é: " . $soma;
-} else if ($media >= 9.5 && $media <= 10) {
-    echo $nome . " você tem " . $idade . " anos. Situação: Aprovado com excelência! A soma de suas notas é: " . $soma;
-} else {
-    echo "Nota inválida.";
+if($pressao < 90){
+    echo "Pressão: X mmHg - Risco: Hipotensão (pressão baixa)";
+}else if($pressao >= 90 && $pressao < 120){
+    echo "Pressão: X mmHg - Situação: Pressão ideal";
+}else if($pressao >= 120 && $pressao < 130){
+    echo "Pressão: X mmHg - Situação: Pressão normal alta";
+}else if($pressao >= 130 && $pressao < 140){
+    echo "Pressão: X mmHg - Situação: Pré-hipertensão (atenção necessária)";
+}else if($pressao >= 140 && $pressao < 160){
+    echo "Pressão: X mmHg - Situação: Hipertensão Estágio 1";
+}else if($pressao >= 160 && $pressao < 180){
+    echo "Pressão: X mmHg - Situação: Hipertensão Estágio 2";
+}else if($pressao >= 180){
+    echo "Pressão: X mmHg - Situação: Crise hipertensiva – risco de vida!";
+}else if($pressao < 0){
+    echo "Valor negativo ou não numérico";
+}else {
+    echo "Digite um valor válido";
 }
-
 ?>
