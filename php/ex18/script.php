516fc8e7ca3b5a8a052b75650f6a507004a27e29
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-06-05 10:45:15 -0300

    Add files via upload

---
+++ php/ex18/script.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$n1 = $_GET['n1'];
+$n2 = $_GET['n2'];
+$n3 = $_GET['n3'];
+$n4 = $_GET['n4'];
+$n5 = $_GET['n5'];
+
+$multi = $n1 * $n2 * $n3 * $n4 * $n5;
+
+echo "A multiplicação dos números é: ".$multi
+?>
