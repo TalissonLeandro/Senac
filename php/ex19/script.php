fbe15ec9481b369b4b7e8e6bc746764b01c7c257
talisson.dourado <166826631+TalissonLeandro@users.noreply.github.com>
2025-06-05 11:07:30 -0300

    Add files via upload

---
+++ php/ex19/script.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero = $_GET['numero'];
+$dobro = $numero * 2;
+$triplo = $numero * 3;
+
+
+echo "Número informado: ".$numero." o dobro do número é: ".$dobro. " o triplo: ".$triplo;
+
+?>
---
+++ php/ex20/script.php
@@ -0,0 +1,9 @@
+<?php
+
+$numero = $_GET['numero'];
+$numero_x7 = $numero * 7;
+
+
+echo "Numero informado: ".$numero." a multiplicação desse número por 7 é: ".$numero_x7;
+
+?>
